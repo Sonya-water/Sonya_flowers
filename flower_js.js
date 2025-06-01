3a7025e00bbe58a075bcec6836c025df23d9adad
Sonya-water <mail@sonyavolkova.ru>
2025-06-01 12:40:30 +0300

    Create flower_js.js

---
+++ flower_js.js
@@ -0,0 +1,8 @@
+function show_flower(){
+	var a = document.getElementById('flo')
+	a.style.display = 'block'
+}
+function no_flower(){
+	var a = document.getElementById('flo')
+	a.style.display = 'none'
+}
